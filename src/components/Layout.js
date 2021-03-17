772905c270e2e3a30b76898d4eab935de4b3398c
AmplifiedHuman <amplifiedhuman@gmail.com>
2021-03-17 19:54:04 +0000

    fixed case related issues

---
+++ src/components/Layout.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Container, Flex, Spacer, Box } from '@chakra-ui/react';
+
+import NavBar from '../components/Navbar';
+
+const Layout = ({ children }) => {
+  return (
+    <Flex direction="column" height="100vh">
+      <NavBar />
+      <Container maxW="6xl" my="8">
+        {children}
+      </Container>
+      <Spacer />
+      <Box>
+        <footer>Sample Footer</footer>
+      </Box>
+    </Flex>
+  );
+};
+
+Layout.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default Layout;
