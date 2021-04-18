1ae86dca77515a3990acdf85408fcd7888da2dba
AmplifiedHuman <amplifiedhuman@gmail.com>
2021-04-18 15:59:36 +0100

    added lazy loading

--- src/components/RemoteImage/index.js
+++ src/components/RemoteImage/index.js
@@ -1,9 +1,20 @@
 import { Image } from '@chakra-ui/image';
-import React from 'react';
+import React, { useState } from 'react';
 
 const RemoteImage = ({ url, ...props }) => {
   const baseURL = 'https://res.cloudinary.com/mysai/image/upload';
-  return <Image src={`${baseURL}/q_auto,f_auto,c_scale/${url}`} {...props} />;
+  const lowQualityOptions = 'e_blur:2000,q_1,f_auto';
+  const highQualityOptions = 'q_auto,f_auto,c_scale';
+
+  const [option, setOption] = useState(lowQualityOptions);
+  return (
+    <Image
+      src={`${baseURL}/${option}/${url}`}
+      {...props}
+      loading="lazy"
+      onLoad={() => setOption(highQualityOptions)}
+    />
+  );
 };
 
 export default RemoteImage;
